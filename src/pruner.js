0a12d6f4601102c622ab914a547acc926d4c61fa
HEADLESS HORSE <github-login@headless.horse>
2024-11-06 15:44:12 +0000

    1.1.1

--- src/pruner.js
+++ src/pruner.js
@@ -11,27 +11,24 @@ function pruner() {
   }));
 
   const processImgs = async (el, vw, vh) => {
-    const { name, cols, roi, scale, path, imageExtension = 'webp' } = JSON.parse(el.dataset.pruner);
+    const { name, cols, roi, scale = "1 0", path, imageExtension = 'webp' } = JSON.parse(el.dataset.pruner);
     if (!cols || !path || !name) return;
-    const [sf, bp] = scale.split(" "), isMobile = vw <= +bp;
-
+    const [sf, bp] = scale.split(" ");
+    const isMobile = vw <= +bp;
     try {
       const { width: w, height: h } = await loadImg(`${path}${name}-1.${imageExtension}`);
       const sw = Math.round(w * (isMobile ? +sf : 1)), sh = Math.round(h * (isMobile ? +sf : 1));
       const numCols = Math.min(Math.ceil(vw / sw), cols), numRows = Math.ceil(vh / sh);
       const c = Object.assign(document.createElement('canvas'), { width: numCols * sw, height: numRows * sh }), ctx = c.getContext('2d');
       el.src = '';
-
       const sR = roi ? Math.max(0, Math.floor((roi - 1) / cols) - Math.floor(numRows / 2)) : Math.floor(numRows / 2);
       const sC = roi ? Math.max(0, (roi - 1) % cols - Math.floor(numCols / 2)) : Math.floor(cols / 2) - Math.floor(numCols / 2);
-
       (await Promise.all(
         Array.from({ length: numRows * numCols }, (_, i) => {
           const r = sR + Math.floor(i / numCols), c = sC + (i % numCols);
           return r < numRows && c < cols ? loadImg(`${path}${name}-${r * cols + c + 1}.${imageExtension}`) : null;
         })
       )).forEach((img, i) => img && ctx.drawImage(img, (i % numCols) * sw, Math.floor(i / numCols) * sh, sw, sh));
-
       el.src = c.toDataURL('image/webp');
     } catch (e) { console.error(e); }
   };
